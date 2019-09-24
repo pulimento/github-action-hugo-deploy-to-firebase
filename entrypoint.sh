865125f3355718e8041f3be83906575800468293
Francisco Javier Pulido Espina <jpulido@plainconcepts.com>
2019-09-24 15:54:59 +0200

    Update files

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,4 @@ hugo
 # Publish to Firebase
 firebase use --token $FIREBASE_DEPLOY_KEY alias
 firebase deploy -m "Trigger: $GITHUB_EVENT_NAME, commit SHA: $GITHUB_SHA" --non-interactive --token $FIREBASE_DEPLOY_KEY
-mkdir .public
-cp -r * .public
-mv .public public
+
